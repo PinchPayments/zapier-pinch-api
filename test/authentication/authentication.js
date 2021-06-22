52ef7316461da03b6e7e9f5292146d65cec847f4
anthonyvscode <anthonyvscode@users.noreply.github.com>
2021-06-22 22:49:53 +1000

    Update authentication.js

--- test/authentication/authentication.js
+++ test/authentication/authentication.js
@@ -7,10 +7,10 @@ const zapier = require('zapier-platform-core');
 const App = require('../../index');
 const appTester = zapier.createAppTester(App);
 
-describe('session auth tests', () => {    
+describe('Session Auth Tests', () => {
   zapier.tools.env.inject();
 
-  it('has an exchange for merchant/secret', (done) => {
+  it('Has an exchange for merchant/secret', (done) => {
     const bundle = {
       authData: {
         merchant_id: process.env.MERCHANT_ID,
@@ -20,39 +20,36 @@ describe('session auth tests', () => {
     };
 
     appTester(App.authentication.sessionConfig.perform, bundle)
-        .then((newAuthData) => {
-          const decodedToken = jwt_decode(newAuthData.access_token);
-          decodedToken.merchant_id.should.eql(bundle.authData.merchant_id.replace("_test", ""))
-        done();
-        })
-    .catch(done);
-
-  });
-
-  it('has auth details added to every request', (done) => {    
-    const bundle = {
-      authData: {
-        merchant_id: process.env.MERCHANT_ID,
-        secret_key: process.env.SECRET_KEY,
-        environment: process.env.ENVIRONMENT
-      }
-    };
-
-    appTester(App.authentication.sessionConfig.perform, bundle)
-    .then((newAuthData) => {
-      bundle.authData.access_token = newAuthData;
-      appTester(App.authentication.test, bundle)
-      .then((response) => {
-        response.status.should.eql(200);
-        response.environment.should.eql(bundle.environment);
+      .then((newAuthData) => {
+        const decodedToken = jwt_decode(newAuthData.access_token);
+        decodedToken.merchant_id.should.eql(bundle.authData.merchant_id.replace("_test", ""))
         done();
       })
       .catch(done);
-    done();
-    })
-.catch(done);
-
 
   });
 
+  it('Has auth details added to every request', (done) => {
+    const bundle = {
+      authData: {
+        merchant_id: process.env.MERCHANT_ID,
+        secret_key: process.env.SECRET_KEY,
+        environment: process.env.ENVIRONMENT
+      }
+    };
+
+    appTester(App.authentication.sessionConfig.perform, bundle)
+      .then((newAuthData) => {
+        bundle.authData.access_token = newAuthData;
+        appTester(App.authentication.test, bundle)
+          .then((response) => {
+            response.status.should.eql(200);
+            response.environment.should.eql(bundle.environment);
+            done();
+          })
+          .catch(done);
+        done();
+      })
+      .catch(done);
+  });
 });
