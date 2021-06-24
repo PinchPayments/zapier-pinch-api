dae16ae69770ba9c002dcfd8060bcf05083e4208
anthonyvscode <anthonyvscode@users.noreply.github.com>
2021-06-24 14:23:12 +1000

    only add auth header if not there already (#4)

--- before-handlers.js
+++ before-handlers.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const includeBearerToken = (request, z, bundle) => {
-  if (bundle.authData.access_token) {
+  if (bundle.authData.access_token && !request.headers.Authorization) {
     request.headers.Authorization = `Bearer ${bundle.authData.access_token}`;
   }
 
