e347a2efaac254a955b83599e22dc1b22038a607
anthonyvscode <anthonyvscode@users.noreply.github.com>
2021-06-25 12:24:38 +1000

    Readme update and added pinch version header (#9)
    * added 2020.1 version header to every call
    
    * updated readme file

--- test/authentication/authentication.js
+++ test/authentication/authentication.js
@@ -42,4 +42,23 @@ describe('Session Auth Tests', () => {
     response.status.should.eql(200);
     response.data.environment.toLowerCase().should.eql(bundle.authData.environment.toLowerCase());
   });
+
+  it('Has API Version added to every request', async () => {
+    const bundle = {
+      authData: {
+        merchant_id: process.env.MERCHANT_ID,
+        secret_key: process.env.SECRET_KEY,
+        environment: process.env.ENVIRONMENT
+      }
+    };
+
+    const newAuthData = await appTester(App.authentication.sessionConfig.perform, bundle);
+    bundle.authData.access_token = newAuthData.access_token;
+
+    var response = await appTester(App.authentication.test, bundle);
+    
+    response.request.headers['pinch-version'].should.eql('2020.1');
+    response.status.should.eql(200);
+    response.data.environment.toLowerCase().should.eql(bundle.authData.environment.toLowerCase());
+  });
 });
